37625314469e825902cc287aab8b17db8f893bda
Mollycodd1e <selik7@mail.ru>
2022-04-27 17:51:53 +0300

    Уменьшает длину файла db.json до 10000 символов

--- src/components/store/api-action.js
+++ src/components/store/api-action.js
@@ -1,14 +1,14 @@
 //import {loadTickets} from "./action";
 
-//export const fetchTicketsList = () => (dispatch, _getState, api) => (
-//  api.get('https://front-test.beta.aviasales.ru/search').then((responce) => console.log(responce))
-//    //.then((response) => api.get(`https://front-test.beta.aviasales.ru/tickets?${response}`)
-//    //.then(({data}) => {
-//    //  console.log(data)
-//    //  const tickets = data.map((ticket,i) => (i < 5) ? adaptOffersToClient(ticket) : '');
-//    //  return tickets;
-//    //})
-//    //.then((tickets) => 
-//    //console.log(tickets),
-//    //dispatch(loadTickets(tickets))))
-//);
+export const fetchTicketsList = () => (dispatch, _getState, api) => (
+  api.get('https://my-json-server.typicode.com/Mollycodd1e/Flexberry').then((responce) => console.log(responce))
+    //.then((response) => api.get(`https://front-test.beta.aviasales.ru/tickets?${response}`)
+    //.then(({data}) => {
+    //  console.log(data)
+    //  const tickets = data.map((ticket,i) => (i < 5) ? adaptOffersToClient(ticket) : '');
+    //  return tickets;
+    //})
+    //.then((tickets) => 
+    //console.log(tickets),
+    //dispatch(loadTickets(tickets))))
+);
